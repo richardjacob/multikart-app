2ceb54b4a54a66f4de35ea90c5845a352b9f8c05
suchi <suchika.parekh@webiots.in>
2021-08-21 15:43:39 +0530

    doc added

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,24 +257,4 @@ localStorage.getItem('dir') ? $('#rtlButton').attr('checked', true) : '';
 
 
 
-/*==========================
- add to home screen popup js
- ==========================*/
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  deferredPrompt = e;
-});
-
-const installApp = document.getElementById('installApp');
-
-installApp.addEventListener('click', async () => {
-  if (deferredPrompt !== null) {
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    if (outcome === 'accepted') {
-      deferredPrompt = null;
-    }
-  }
-});
 
